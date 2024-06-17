25546412c82d24e3216ed8fbaf869c23a6fcf536
Dominic DiModugno <ddimodugno@Len-Searfoss.local>
2024-06-17 15:59:50 -0500

    delete invoice functionality

---
+++ app/lib/actions.ts
@@ -0,0 +1,40 @@
+'use server';
+
+import { z } from 'zod'; 
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+// Some validation library Next.js docs recommended for form data validation
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+})
+
+// Makes it so that you don't need to pass the ID or DATE fields to the object in order to validate. 
+const CreateInvoice = FormSchema.omit({ id: true, date: true }); 
+ 
+export async function createInvoice(formData: FormData) {
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+  const amountInCents = amount * 100; 
+  const date = new Date().toISOString().split('T')[0];
+
+  // Next.js magically knows where and how to send this to the db through the .env file?
+  await sql`
+  INSERT INTO invoices (customer_id, amount, status, date)
+  VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `; 
+
+  // Clears the Client-Side Router Cache. 
+  revalidatePath('/dashboard/invoices');
+
+  // Self Explanatory...
+  redirect('/dashboard/invoices');
+}
