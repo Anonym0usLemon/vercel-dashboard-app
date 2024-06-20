5e2c7194591608285f87fded1fa8780d991b3e52
Dominic DiModugno <anonym0uslemon@Dominics-Laptop.local>
2024-06-19 23:45:06 -0500

    form validation

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -62,6 +62,7 @@ export default function EditInvoiceForm({
                 name="amount"
                 type="number"
                 step="0.01"
+                max="999999"
                 defaultValue={invoice.amount}
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
